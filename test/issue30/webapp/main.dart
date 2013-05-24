0b68b66f36c137b2fcb457f78b5a232d5a1fedd2
tomyeh <tomyeh@potix.com>
2013-05-24 09:34:17 +0800

    add example of mapping static resources to particular prefix

---
+++ test/issue30/webapp/main.dart
@@ -0,0 +1,15 @@
+//Copyright (C) 2013 Potix Corporation. All Rights Reserved.
+//History: Sun, May 19, 2013  1:51:09 PM
+// Author: tomyeh
+
+import "package:stream/stream.dart";
+
+void main() {
+  new StreamServer(
+    uriMapping: {
+      "/static/.*": (connect) => connect.server.resourceLoader
+        .load(connect, connect.request.uri.path.substring(7)),
+      "/": (connect) => connect.forward("/static/test.html"),
+      "/.*": (connect) => throw new Http404(connect.request.uri.path)
+    }).start();
+}

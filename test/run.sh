1281c7edbdee1adb11dfd18a4a3c0ceaab298367
tomyeh <tomyeh@potix.com>
2013-06-04 17:48:14 +0800

    use dartanalyzer instead

--- test/run.sh
+++ test/run.sh
@@ -5,7 +5,7 @@ set -e
 
 DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
 
-# Note: dart_analyzer needs to be run from the root directory for proper path
+# Note: dartanalyzer needs to be run from the root directory for proper path
 # canonicalization.
 pushd $DIR/..
 echo Compile RSP files
@@ -13,12 +13,12 @@ find -name *.rsp.dart | xargs rm -rf
 dart bin/rspc.dart */*/*.rsp.html
 
 echo Analyzing library for warnings or type errors
-dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
+dartanalyzer --fatal-warnings --fatal-type-errors lib/*.dart \
   || echo -e "Ignoring analyzer errors"
 
 for fn in `grep -l 'main[(][)]' */*/*/*.dart|grep -v packages/`; do
 	echo Analyzing $fn
-	dart_analyzer --fatal-warnings --fatal-type-errors lib/*.dart \
+	dartanalyzer --fatal-warnings --fatal-type-errors lib/*.dart \
 	  || echo -e "Ignoring analyzer errors"
 done
 
